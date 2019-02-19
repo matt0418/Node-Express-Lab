85622b3dc02ef2e042f8af23e786777dfa3323b1
matt0418 <mfeldma7@gmail.com>
2019-02-19 16:32:01 -0500

    Added get method for /posts endpoint

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
+const postsRouter = require('./routes/posts')
+
 const server = express()
 
 server.use(express.json())
+server.use('/api/posts', postsRouter)
 
 server.get('/', (req, res) => {
     res.send(`
