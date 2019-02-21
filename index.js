4aecfdcde4ff4a86a7b42d55a2d16f68ade48d9c
matt0418 <mfeldma7@gmail.com>
2019-02-21 16:25:49 -0500

    Fixing heroku deployment

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const path = require('path')
 const port = process.env.PORT || 4000
 
 server.use(express.static(path.join(_dirname, 'client/build')))
+
+if(process.env.NODE_ENV === 'production') {
+    server.use(express.static(path.join(__dirname, 'client/build')));
+    //
+    server.get('*', (req, res) => {
+      res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    })
+  }
+
 server.get('*', (req, res) => {
     res.sendFile(path.join(_dirname+'/client/public/index.html'))
 })
